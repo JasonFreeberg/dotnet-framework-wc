3b8b41b8977def3b5a03541ac6296a00723debe4
Jeff Martinez <jeffxmartinez@gmail.com>
2020-03-05 17:57:56 -0800

    work in progress

---
+++ dotnetwindowsapp/dotnetwindowsapp/Controllers/TaskItemController.cs
@@ -0,0 +1,136 @@
+﻿using dotnetwindowsapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotnetwindowsapp.Controllers
+{
+    public class TaskItemController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Todos
+        public ActionResult Index()
+        {
+            Trace.WriteLine("GET /Todos/Index");
+            return View(db.Tasks.ToList());
+        }
+
+        // GET: Todos/Details/5
+        public ActionResult Details(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Details/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem todo = db.Tasks.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Create
+        public ActionResult Create()
+        {
+            Trace.WriteLine("GET /Todos/Create");
+            //return View(new TaskItem { CreatedDate = DateTime.Now });
+            return View();
+        }
+
+        // POST: Todos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Description,CreatedDate")] TaskItem todo)
+        {
+            Trace.WriteLine("POST /Todos/Create");
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(todo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        // GET: Todos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Edit/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem todo = db.Tasks.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Description,CreatedDate")] TaskItem todo)
+        {
+            Trace.WriteLine("POST /Todos/Edit/" + todo.ID);
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Delete/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem todo = db.Tasks.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trace.WriteLine("POST /Todos/Delete/" + id);
+            TaskItem todo = db.Tasks.Find(id);
+            db.Tasks.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ dotnetwindowsapp/dotnetwindowsapp/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace dotnetwindowsapp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=MyAzureConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TaskItem> Tasks { get; set; }
+    }
+}
